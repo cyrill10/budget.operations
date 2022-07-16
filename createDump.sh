68c18fe1d98c673baf2118c80ea5be3a6831e856
cyrill10 <cyrill.bader@gmx.ch>
2022-07-17 00:41:18 +0200

    connect to mongodb

--- createDump.sh
+++ createDump.sh
@@ -1,3 +1,11 @@
- mysqldump  -h 0.0.0.0 -u root -pbudget-app budget > /home/budget-server/dumps/`date '+%Y-%m-%d'`.sql
-/home/budget-server/Dropbox-Uploader/dropbox_uploader.sh upload /home/budget-server/dumps/`date '+%Y-%m-%d'`.sql /
-rm /home/budget-server/dumps/`date '+%Y-%m-%d'`.sql
+#!/bin/sh
+export $(grep -v '^#' .env | xargs)
+echo ${MONGO_URI}
+
+mongoexport --uri=${MONGO_URI} -o /home/budget-server/dumps/realAccount_`date '+%Y-%m-%d'`.txt -d budget -c realAccount --jsonArray
+mongoexport --uri=${MONGO_URI} -o /home/budget-server/dumps/virtualAccount_`date '+%Y-%m-%d'`.txt -d budget -c virtualAccount --jsonArray
+mongoexport --uri=${MONGO_URI} -o /home/budget-server/dumps/transaction_`date '+%Y-%m-%d'`.txt -d budget -c transaction --jsonArray
+mongoexport --uri=${MONGO_URI} -o /home/budget-server/dumps/closingProcess_`date '+%Y-%m-%d'`.txt -d budget -c closingProcess --jsonArray
+mongoexport --uri=${MONGO_URI} -o /home/budget-server/dumps/scannedTransaction_`date '+%Y-%m-%d'`.txt -d budget -c scannedTransaction --jsonArray
+/home/budget-server/Dropbox-Uploader/dropbox_uploader.sh upload /home/budget-server/dumps/* /
+rm /home/budget-server/dumps/*

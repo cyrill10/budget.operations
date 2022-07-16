21e49d77a70a66f7b771d1739ae7398f40e9fd89
cyrill10 <cyrill.bader@gmx.ch>
2022-07-17 01:00:42 +0200

    log uri

--- createDump.sh
+++ createDump.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 export $(grep -v '^#' .env | xargs)
+echo${MONGO_URI_WITH_DATABASE}
 
 mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/realAccount_`date '+%Y-%m-%d'`.txt -c realAccount --jsonArray
 mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/virtualAccount_`date '+%Y-%m-%d'`.txt -c virtualAccount --jsonArray
 mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/transaction_`date '+%Y-%m-%d'`.txt -c transaction --jsonArray
 mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/closingProcess_`date '+%Y-%m-%d'`.txt -c closingProcess --jsonArray
 mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/scannedTransaction_`date '+%Y-%m-%d'`.txt -c scannedTransaction --jsonArray
-/home/budget-server/Dropbox-Uploader/dropbox_uploader.sh upload /home/budget-server/dumps/* /
+/home/budget-server/Dropbox-Uploader/dropbox_uploader.sh upload /home/budget-server/dumps/*.txt /
 rm /home/budget-server/dumps/*

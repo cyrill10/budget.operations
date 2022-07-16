f06a737834ba85cdbf3fcd78b0555ced065fdd51
cyrill10 <cyrill.bader@gmx.ch>
2022-07-17 00:58:32 +0200

    fix variable with db

--- createDump.sh
+++ createDump.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 export $(grep -v '^#' .env | xargs)
-echo ${MONGO_URI}
 
-mongoexport --uri=${MONGO_URI} -o /home/budget-server/dumps/realAccount_`date '+%Y-%m-%d'`.txt -d budget -c realAccount --jsonArray
-mongoexport --uri=${MONGO_URI} -o /home/budget-server/dumps/virtualAccount_`date '+%Y-%m-%d'`.txt -d budget -c virtualAccount --jsonArray
-mongoexport --uri=${MONGO_URI} -o /home/budget-server/dumps/transaction_`date '+%Y-%m-%d'`.txt -d budget -c transaction --jsonArray
-mongoexport --uri=${MONGO_URI} -o /home/budget-server/dumps/closingProcess_`date '+%Y-%m-%d'`.txt -d budget -c closingProcess --jsonArray
-mongoexport --uri=${MONGO_URI} -o /home/budget-server/dumps/scannedTransaction_`date '+%Y-%m-%d'`.txt -d budget -c scannedTransaction --jsonArray
+mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/realAccount_`date '+%Y-%m-%d'`.txt -c realAccount --jsonArray
+mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/virtualAccount_`date '+%Y-%m-%d'`.txt -c virtualAccount --jsonArray
+mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/transaction_`date '+%Y-%m-%d'`.txt -c transaction --jsonArray
+mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/closingProcess_`date '+%Y-%m-%d'`.txt -c closingProcess --jsonArray
+mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/scannedTransaction_`date '+%Y-%m-%d'`.txt -c scannedTransaction --jsonArray
 /home/budget-server/Dropbox-Uploader/dropbox_uploader.sh upload /home/budget-server/dumps/* /
 rm /home/budget-server/dumps/*

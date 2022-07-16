a84b5cab72584c9904e33ce95deba218737ada06
cyrill10 <cyrill.bader@gmx.ch>
2022-07-17 01:26:47 +0200

    fix uri

--- createDump.sh
+++ createDump.sh
@@ -2,7 +2,7 @@
 export $(grep -v '^#' .env | xargs)
 echo ${MONGO_URI_WITH_DATABASE}
 
-mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/realAccount.txt -c realAccount --jsonArray
+mongoexport --uri=${MONGO_URI_WITH_DATABASE} --verbose -o /home/budget-server/dumps/realAccount_`date '+%Y-%m-%d'`.txt -c realAccount --jsonArray
 mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/virtualAccount_`date '+%Y-%m-%d'`.txt -c virtualAccount --jsonArray
 mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/transaction_`date '+%Y-%m-%d'`.txt -c transaction --jsonArray
 mongoexport --uri=${MONGO_URI_WITH_DATABASE} -o /home/budget-server/dumps/closingProcess_`date '+%Y-%m-%d'`.txt -c closingProcess --jsonArray

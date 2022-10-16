a8b5fcdc3cb1c6be268a50439f3a833b51190320
cyrill10 <cyrill.bader@gmx.ch>
2022-10-16 12:18:08 +0200

    update dump format

--- createDump.sh
+++ createDump.sh
@@ -2,10 +2,10 @@
 export $(grep -v '^#' /home/budget-server/.env | xargs)
 echo ${MONGO_URI_WITH_DATABASE}
 
-mongoexport --uri=${MONGO_URI_WITH_DATABASE} --forceTableScan -o /home/budget-server/dumps/realAccount_`date '+%Y-%m-%d'`.txt -c realAccount --jsonArray
-mongoexport --uri=${MONGO_URI_WITH_DATABASE} --forceTableScan -o /home/budget-server/dumps/virtualAccount_`date '+%Y-%m-%d'`.txt -c virtualAccount --jsonArray
-mongoexport --uri=${MONGO_URI_WITH_DATABASE} --forceTableScan -o /home/budget-server/dumps/transaction_`date '+%Y-%m-%d'`.txt -c transaction --jsonArray
-mongoexport --uri=${MONGO_URI_WITH_DATABASE} --forceTableScan -o /home/budget-server/dumps/closingProcess_`date '+%Y-%m-%d'`.txt -c closingProcess --jsonArray
-mongoexport --uri=${MONGO_URI_WITH_DATABASE} --forceTableScan -o /home/budget-server/dumps/scannedTransaction_`date '+%Y-%m-%d'`.txt -c scannedTransaction --jsonArray
+mongoexport --uri=${MONGO_URI_WITH_DATABASE} --forceTableScan -o /home/budget-server/dumps/`date '+%Y-%m-%d'`_realAccount.txt -c realAccount --jsonArray
+mongoexport --uri=${MONGO_URI_WITH_DATABASE} --forceTableScan -o /home/budget-server/dumps/`date '+%Y-%m-%d'`_virtualAccount.txt -c virtualAccount --jsonArray
+mongoexport --uri=${MONGO_URI_WITH_DATABASE} --forceTableScan -o /home/budget-server/dumps/`date '+%Y-%m-%d'`_transaction.txt -c transaction --jsonArray
+mongoexport --uri=${MONGO_URI_WITH_DATABASE} --forceTableScan -o /home/budget-server/dumps/`date '+%Y-%m-%d'`_closingProcess.txt -c closingProcess --jsonArray
+mongoexport --uri=${MONGO_URI_WITH_DATABASE} --forceTableScan -o /home/budget-server/dumps/`date '+%Y-%m-%d'`_scannedTransaction.txt -c scannedTransaction --jsonArray
 /home/budget-server/Dropbox-Uploader/dropbox_uploader.sh upload /home/budget-server/dumps/*.txt /
 rm /home/budget-server/dumps/*
